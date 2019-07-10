21fb4cab929068bcf62e50629712463ac46dbc72
Nicolas Kijak <nkijak@gmail.com>
2019-07-10 13:02:02 -0400

    patching i3config to work without gaps

--- i3/install.sh
+++ i3/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-ln -svf ~/.dotfiles/i3/i3 ~/.config/i3
-ln -svf ~/.dotfiles/i3/i3blocks ~/.config/i3blocks
+if [ -d ~/.config/i3 ]; then
+  echo "i3 config exists, moving out of the way..."
+  mv ~/.config/i3 ~/.config/i3.bak
+fi
+ln -svf ~/.dotfiles/i3/i3/ ~/.config/
+ln -svf ~/.dotfiles/i3/i3blocks/ ~/.config/
---
+++ linux/ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TODO make this an array of packages
+
+# install basics
+apt install -y build-essentials
+
+# install i3-wm
+apt install -y i3 i3blocks
